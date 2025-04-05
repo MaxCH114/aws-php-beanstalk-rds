4d6dc429d262cb318f39d8c31c7973644c752ffc
Max114 <paulmaxchamblain@gmail.com>
2025-04-05 19:30:34 -0400

    Beanstalk and Rds creation

--- Beanstalk-rds/provider.tf
+++ Beanstalk-rds/provider.tf
@@ -1,18 +1,3 @@
-
-# This file is used to configure the provider and backend for the terraform code.
-# The provider is the plugin that terraform uses to interact with the cloud provider.
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-
-}
-
 provider "aws" {
-  region = var.region
-}provider "aws" {
+  region     = "us-east-1"
+}
